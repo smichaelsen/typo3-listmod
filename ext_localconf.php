bcf884e007e88af227b3faefdbc5cb785dfff74a
Sebastian Michaelsen <sebastian.michaelsen@t3seo.de>
2013-12-18 10:52:29 +0100

    Enable / Disable controls in List Module via TSconfig

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,3 +1,4 @@
 <?php
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/class.db_list_extra.inc']['getTable']['listmod'] = 'T3SEO\\Listmod\\Hook\\ListModule';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/class.db_list_extra.inc']['actions']['listmod'] = 'T3SEO\\Listmod\\Hook\\ListModule';
